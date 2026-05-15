145a78fa2196a283b9df03381dbd137442b2415f
Jhener Albarado Mamani <jhener.albarado@goumh.umh.es>
2026-05-15 02:47:36 +0200

    Segundo Test: agregar y buscar equipos en la base de datos.

---
+++ src/main/java/todolist/service/EquipoService.java
@@ -0,0 +1,9 @@
+package todolist.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EquipoService {
+    Logger logger = LoggerFactory.getLogger(EquipoService.class);
+
+}
