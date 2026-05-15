01b663231e9ccddbac38f60a7a347c0e9009aff0
Jhener Albarado Mamani <jhener.albarado@goumh.umh.es>
2026-05-15 09:20:00 +0200

    Tercer commit - Definicion de igualdad entre equipos

--- src/test/java/todolist/repository/EquipoTest.java
+++ src/test/java/todolist/repository/EquipoTest.java
@@ -27,7 +27,7 @@ public class EquipoTest {
     public void grabarYBuscarEquipo(){
         //Given
         //Un quipo nuevo
-        Equipo equipo = new Equipo("Project P1");
+        Equipo equipo;
 
         //Probamos el producto vacio, necesario para que funcione JPA/hibernte
         Equipo equipo1 = new Equipo();
@@ -45,5 +45,32 @@ public class EquipoTest {
         Equipo equipoDB = equipoRepository.findById(equipoId).orElse(null);
         assertThat(equipoDB).isNotNull();
         assertThat(equipoDB.getNombre()).isEqualTo("Project P1");
+    }
+
+    @Test
+    public void comprobarIgualdadEquipos(){
+        //GIVEN
+        //Creamos 3 equipos sin ID solo con el nombre
+        Equipo equipo1 = new Equipo("Project 1");
+        Equipo equipo2 = new Equipo("Project 2");
+        Equipo equipo3 = new Equipo("Project 2");
+
+        //THEN
+        // Comprobamos igualdad basada en el atributo nombre y que el
+        // hashCode es el mismo para dos equipos con igual nombre
+        assertThat(equipo1).isNotEqualTo(equipo2);
+        assertThat(equipo2).isEqualTo(equipo3);
+        assertThat(equipo2.hashCode()).isEqualTo(equipo3.hashCode());
+
+        //WHEN
+        //Añadimos identificadores y comprobamos igualdad por identificadores
+        equipo1.setId(1L);
+        equipo2.setId(1L);
+        equipo3.setId(3L);
+
+        //WHEN
+        //Comprobamos igualdad basada en el atributo nombre
+        assertThat(equipo1).isEqualTo(equipo2);
+        assertThat(equipo2).isNotEqualTo(equipo3);
     }
 }
